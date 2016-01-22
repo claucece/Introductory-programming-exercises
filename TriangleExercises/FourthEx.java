041404636c0e8825f08f65e9c00a9ee5df0d0023
Sofia Celi <sceli@thoughtworks.com>
2016-01-22 17:08:05 -0500

    fixed indentation

--- TriangleExercises/FourthEx.java
+++ TriangleExercises/FourthEx.java
@@ -11,11 +11,11 @@ public class FourthEx {
         fourth.printTriangle(9);
     }
 
-    private void printTriangle(int n){
+    private void printTriangle(int n) {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) 
-            System.out.print("*");
-            System.out.println();
+            for (int j = 0; j <= i; j++) {
+            System.out.print("*"); }
+        System.out.println();
         }
     }
 }
