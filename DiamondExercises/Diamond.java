e5077e19aca3d72ff450d8e6c86d18660375d4b1
Sofia Celi <sceli@thoughtworks.com>
2016-01-22 11:41:22 -0500

    changed methods' names

--- DiamondExercises/Diamond.java
+++ DiamondExercises/Diamond.java
@@ -7,31 +7,31 @@ public class Diamond {
     public static void main(String[] args) {
         System.out.println("Let's make a Diamond out of it:");
         Diamond gem = new Diamond();
-        gem.makeFirst(4);
+        gem.setFirst(4);
     }
 
-    private void makeFirst(int height) {
+    private void setFirst(int height) {
         int base = (2 * height) - 1;
         for (int i = 0; i < height; i++) {
             int asterisk = (2 * i) + 1;
             int space = (base - asterisk) / 2;
-            printDiamond(space, asterisk);
+            getFunctionality(space, asterisk);
             System.out.println();
         }
         int minus = height - 1;
-        makeSecond(minus, base);
+        setSecond(minus, base);
     }
   
-    private void makeSecond(int minus, int base) {
+    private void setSecond(int minus, int base) {
         for (int i = minus; i > 0; i--) {
             int asterisk = (2 * i) - 1;
             int space = (base - asterisk) / 2;
-            printDiamond(space, asterisk);
+            getFunctionality(space, asterisk);
             System.out.println();
         }
     }
  
-    private void printDiamond(int space, int asterisk) {
+    private void getFunctionality(int space, int asterisk) {
         for (int j = 0; j < space; j++)
             System.out.print(" ");
         for (int k = 0; k < asterisk; k++)
