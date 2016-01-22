baf09503ffe1c6320ae9e520babf0b670b6bc910
Sofia Celi <sceli@thoughtworks.com>
2016-01-22 11:44:08 -0500

    methods name changed

--- DiamondExercises/IsoscelesTriangle.java
+++ DiamondExercises/IsoscelesTriangle.java
@@ -7,20 +7,20 @@ public class IsoscelesTriangle {
     public static void main(String[] args) {
         System.out.println("Let's make an isosceles Triangle out of it:");
         IsoscelesTriangle isosceles = new IsoscelesTriangle();
-        isosceles.defineTriangle(5);
+        isosceles.setTriangle(5);
     }
 
-    private void defineTriangle(int height) {
+    private void setTriangle(int height) {
        int base = (2 * height) - 1;
        for (int i = 0; i < height; i++) {
            int asterisk = (2 * i) + 1;
            int space = (base - asterisk) / 2;
-           printTriangle(space, asterisk);
+           getTriangle(space, asterisk);
            System.out.println();
        }  
     }
  
-    private void printTriangle(int space, int asterisk) {
+    private void getTriangle(int space, int asterisk) {
       for (int j = 0; j < space; j++)
            System.out.print(" ");
       for (int k = 0; k < asterisk; k++)
